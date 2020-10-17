f5827c7e4a56502aaa293c4c83c116da9219f345
Ravi Ganesan <ravi@findravi.com>
2020-10-16 19:52:00 -0700

    Test Push
    Homework 3 being submitted for testing.

--- README.md
+++ README.md
@@ -1,93 +1,19 @@
-# 03 JavaScript: Password Generator
+# bootcamp-hw-3
 
-## Your Task
+## Homework Assignment 3
 
-This week’s homework requires you to create an application that an employee can use to generate a random password based on criteria they’ve selected by modifying starter code. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished user interface that is responsive, ensuring that it adapts to multiple screen sizes.
+In this homework assignment I created a random password generator.  The method of implementation I chose (I work in Cybersecurity!) sacrifices a little bit of efficiency to ensure the password is truly as random as permitted by the underlying Javascript random function used. It would have been simplea tiny bit more efficient  to pick the categories at random, and then select a character at random from the chosen catgory. However, this is not quite correct, as the probability of selecting a category would then need to be wreighted by the number of characters in a category, which would have added more complexity. 
 
-The password can include special characters. If you’re unfamiliar with these, see this [list of Password Special Characters from the OWASP Foundation](https://www.owasp.org/index.php/Password_special_characters).
+The  primary areas for improvement in this project are:
+- A proper implementation would not use confirm/prompt for selection of rules, and that should be replaced by Jquery.
+- Rather than use math.Random, for this application it would have been better to use Crypto.getRandomValues(). 
+- In retrospect it would have been better to create a generatePassword object to encapsulate the resulting functions.
 
-## User Story
+The index.html, script.js and style.css pages provided are the only files. Only the script.js was edited, as what was provided met the rest of the specs and was responsive.
 
-```
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
-```
+## Deployed To:
+The resulting web page has been deployed to:
+https://ravifindravicom.github.io/bootcamp-hw-3/
 
-## Acceptance Criteria
-
-```
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
-
-## Mock-Up
-
-The following image shows the web application's appearance and functionality:
-
-![password generator demo](./Assets/03-javascript-homework-demo.png)
-
-## Grading Requirements
-
-This homework is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the above acceptance criteria plus the following:
-
-  * The homework should not produce any errors in the console when you inspect it using Chrome DevTools.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the homework instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality README file with description, screenshot, and link to deployed application.
-
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-- - -
-© 2020 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+## Image
+![alt "Random Password Generator Page Graphic"](./Random_Password_Gen.png?raw=true "Raandom Password Generator")
--- script.js
+++ script.js
@@ -5,55 +5,99 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
+  
 }
 
+/* The generatePassword() function first creates a character pallete based on user choices, then randomly selects passowrd of appropriate size from pallete. We recheck to ensure rules were met as the random selection may not always include all selections. */
 function generatePassword() {
- var passwordLength;
-  /*Variable would have sufficed instead of array in passwordChoice. */
+  var getPassword = [];
+  var passwordStr ="";
   var passwordChoices = [false,false, false, false];
+  const charArray = ["abcdefghijklmnopqrstuvwxyz","ABCDEFGHIJKLMNOPQRSTUVWXYZ"," !\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~","0123456789"];
+  var palleteStr = pallete(passwordChoices,charArray);
+  var passLength = getPasswordLength();
+  var passRuleCheck = false;
+ do {
+    for(i=0; i<passLength; i++)
+        {
+          var rand0to1 = Math.random();
+          var randScaledUp = Math.floor(rand0to1 * palleteStr.length);
+          getPassword[i] = palleteStr[randScaledUp];
+          passwordStr=passwordStr+getPassword[i];
+      }
+    passRuleCheck = checkRules(getPassword,passwordChoices,charArray);
+  } while(passRuleCheck === false);
+  return(passwordStr);
+}
+
+/* The checkRules function simply rechecks to ensure rules were met. */
+function checkRules(getPassword,passwordChoices,charArray){
+  var status = false;
+  var subStatus = [0,0,0,0];
+  var numChoicesMade = 0;
+  var numChoicesFound = 0;
+  for(i=0;i<4;i++){
+    if(passwordChoices[i] === true){
+      numChoicesMade++;
+      for(j=0;j<getPassword.length;j++){
+        for(k=0;k<charArray[i].length;k++){
+          if(getPassword[j]===charArray[i][k]) {
+            subStatus[i]++;
+          }
+        }
+      }
+      if(subStatus[i] > 0){
+        numChoicesFound++;
+      } 
+    }
+  }
+  if(numChoicesMade === numChoicesFound) {
+    return(true);
+  } else {
+    return(false);
+  }
+}
+
+/* This function constructs the character selection pallete based on user choices. */
+function pallete(passwordChoices,charArray){
   var atLeastOneChar = false;
-  /* Note for OWASP special characters one needs to escape the double quote and back slash. */
-  const splCharFromOWASP = " !\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~";
-  const charNumerals = "0123456789";
-  const charUpper ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const charLower ="abcdefghijklmnopqrstuvwxyz";
-  var charPallete = splCharFromOWASP + charNumerals + charUpper + charLower;
-  var passwordLength = 0;
-  var randPassword;
-
-  console.log(charPallete);
-
+  var palleteStr="";
+  atLeastOneChar = passwordChoices[0] || passwordChoices[1] || passwordChoices[2];
   do {
-    passwordChoices[0] = confirm("Do you want at least one lowercase character?");
-    passwordChoices[1] = confirm("Do you want at least one uppercase character?");
-    passwordChoices[2] = confirm("Do you want at least one special character?");
-    atLeastOneChar = passwordChoices[0] || passwordChoices[1] || passwordChoices[2];
-    if (atLeastOneChar == false) { 
-      alert("You need at least one character; lets try again");
-   }
-  }
+  /* Note using confirm and alert should preferably be replaced by jQuery form with radio buttons */
+      passwordChoices[0] = confirm("Do you want lowercase characters?");
+      passwordChoices[1] = confirm("Do you want uppercase characters?");
+      passwordChoices[2] = confirm("Do you want special characters?");
+      atLeastOneChar = passwordChoices[0] || passwordChoices[1] || passwordChoices[2];
+      if (atLeastOneChar == false) { 
+        alert("You need at least one character type; lets try again");
+     }
+    }
   while (atLeastOneChar == false);
-  passwordChoices[3] = confirm("Do you want at least one numeral?");
-  do {
-    console.log("Inside last while loop")
-    passwordLength = prompt("Choose a length from 8 to 128 characters");
+  passwordChoices[3] = confirm("Do you want numerals?");
+  for(i=0; i<4; i++) {
+      if(passwordChoices[i])
+          palleteStr = palleteStr + charArray[i];
   }
-  while((passwordLength <= 8) || (passwordLength >= 128))
+return(palleteStr);
+}
 
-  for (i=0;i<passwordLength;i++)
-  {
-    var rand0to1 = Math.random();
-    console.log(rand0to1);
-    var randScaledUp = Math.floor(rand0to1 * charPallete.length);
-    console.log(randScaledUp);
-    var randPassword[i] = charPallete[randScaledUp];
-    console.log("-"+randPassword[i]);
-  }
- return(randPassword);
+/* getPasswordLength gets the user's desired password length. */
+function getPasswordLength() {
+var passwordStr;
+var passwordLength;
+do {
+  passwordStr = prompt("Choose a length from 8 to 128 characters");
+  passwordLength = parseInt(passwordStr,10)
+}
+while((passwordLength < 8) || (passwordLength > 128));
+return(passwordLength);
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword());
